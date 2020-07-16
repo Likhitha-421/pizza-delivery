fb29ae0c6c97ff19959f61e4bb60838df1635b6c
Davronbek Achilov <davron211@gmail.com>
2020-07-16 14:45:45 +0500

    Responsive fixes

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,10 +1,16 @@
 <template>
     <div v-if="category" :id="'menu-' + category.id" class="app-container">
-        <h2 style="text-align: left; margin-bottom: 20px; color: #ff6900;">
-            {{ category.name }}
-        </h2>
+        <h2 style="text-align: left; margin-bottom: 20px; color: #ff6900;">{{ category.name }}</h2>
         <el-row>
-            <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="6" v-for="(item, index) in items" :key="index">
+            <el-col
+                    :xs="24"
+                    :sm="12"
+                    :md="12"
+                    :lg="8"
+                    :xl="6"
+                    v-for="(item, index) in items"
+                    :key="index"
+            >
                 <div class="grid-content">
                     <article class="item-article">
                         <main class="main-item">
@@ -15,30 +21,22 @@
                                         :alt="item.name"
                                         :title="item.name"
                                         type="1"
-                                        data-ll-status="loaded"/>
+                                        data-ll-status="loaded"
+                                />
                             </figure>
                             <h2 class="product-title">{{ item.name }}</h2>
-                            <span>
-                                {{ item.title }}
-                            </span>
+                            <span>{{ item.title }}</span>
                         </main>
                         <footer class="pizza-footer" style="background-color: #ffffff;">
                             <div class="price-item">
-                                from <span class="money">
-                                <span class="value">
-                                    {{ getCurrentPrice(item.prices) }}
-                                </span>
-                                <span class="on-the-right">
-                                    &nbsp;{{ selectedCurrency }}
-                                </span>
-                            </span>
+                                from
+                                <span class="money">
+                  <span class="value">{{ getCurrentPrice(item.prices) }}</span>
+                  <span class="on-the-right">&nbsp;{{ selectedCurrency }}</span>
+                </span>
                             </div>
                             <div class="pizza-select">
-                                <el-button
-                                        type="success"
-                                        @click="handleSelect(item)"
-                                        round>Select
-                                </el-button>
+                                <el-button type="success" @click="handleSelect(item)" round>Select</el-button>
                             </div>
                         </footer>
                     </article>
@@ -52,7 +50,9 @@
                             :title="form.name"
                             :visible.sync="centerDialogVisible"
                             width="60%"
-                            center>
+                            class="dialog-mobile"
+                            center
+                    >
                         <el-row>
                             <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" style="text-align: center;">
                                 <div>
@@ -60,45 +60,49 @@
                                         <img
                                                 :src="form.img"
                                                 height="292"
-                                                alt=""
+                                                max-width="100%"
+                                                style="object-fit: contain"
+                                                alt
                                                 type="1"
-                                                data-ll-status="loaded"/>
+                                                data-ll-status="loaded"
+                                        />
                                     </figure>
                                 </div>
                             </el-col>
                             <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
                                 <div>
                                     <h3>{{ form.name }}</h3>
-                                    <br>
+                                    <br />
                                     <span>{{ form.title }}</span>
                                     <p>{{ form.description }}</p>
-                                    <br>
+                                    <br />
                                     <div>
-                                        <strong>Price:</strong> {{ getCurrentPrice(form.prices) }} {{ selectedCurrency }}
+                                        <strong>Price:</strong>
+                                        {{ getCurrentPrice(form.prices) }} {{ selectedCurrency }}
                                     </div>
-                                    <br>
+                                    <br />
                                     <el-radio-group class="size-select" v-model="form.type" size="small">
                                         <el-radio-button label="1">Small</el-radio-button>
                                         <el-radio-button label="2">Middle</el-radio-button>
                                         <el-radio-button label="3">Big</el-radio-button>
                                     </el-radio-group>
-                                    <br>
+                                    <br />
                                     <el-input-number v-model="num" :step="2"></el-input-number>
-                                    <p><strong>Total price:</strong> {{ totalPrice }} &nbsp; {{ selectedCurrency }}</p>
+                                    <p>
+                                        <strong>Total price:</strong>
+                                        {{ totalPrice }} &nbsp; {{ selectedCurrency }}
+                                    </p>
                                 </div>
                             </el-col>
                         </el-row>
                         <span slot="footer" class="dialog-footer">
-                            <el-button @click="centerDialogVisible = false">
-                                <i class="el-icon-close" /> Cancel
-                            </el-button>
-                            <el-button
-                                    type="primary"
-                                    @click="confirmCart"
-                                    :disabled="disableAddCart">
-                                <i class="el-icon-plus" /> Add to Shopping Cart
-                            </el-button>
-                        </span>
+              <el-button @click="centerDialogVisible = false">
+                <i class="el-icon-close" /> Cancel
+              </el-button>
+              <el-button type="primary" @click="confirmCart" :disabled="disableAddCart">
+                <i class="el-icon-plus" /> Add to Shopping Cart
+              </el-button>
+            </span>
                     </el-dialog>
                 </div>
             </el-col>
@@ -107,15 +111,15 @@
 </template>
 
 <script>
-    import { fetchList } from '../../api/item'
-    import { mapActions, mapGetters } from 'vuex'
+    import { fetchList } from "../../api/item";
+    import { mapActions, mapGetters } from "vuex";
     export default {
         name: "MenuFoods",
         props: {
             category: {
                 type: Object,
-                default () {
-                    return {}
+                default() {
+                    return {};
                 }
             }
         },
@@ -126,71 +130,69 @@
                 centerDialogVisible: false,
                 totalPrice: 0,
                 form: {
-                    id: '',
+                    id: "",
                     qty: 1,
-                    img: '',
-                    prices: {
-                    },
-                    title: '',
-                    description: '',
-                    type: 1,
+                    img: "",
+                    prices: {},
+                    title: "",
+                    description: "",
+                    type: 1
                 },
                 items: []
-            }
+            };
         },
         computed: {
-            ...mapGetters([
-                'selectedCurrency'
-            ]),
+            ...mapGetters(["selectedCurrency"])
         },
-        created () {
-            this.init()
+        created() {
+            this.init();
         },
         methods: {
-            ...mapActions('cart', [
-                'addToCart'
-            ]),
+            ...mapActions("cart", ["addToCart"]),
             handleSelect(item) {
-                this.form = item
-                this.form.qty = 1
-                this.centerDialogVisible = true
+                this.form = item;
+                this.form.qty = 1;
+                this.centerDialogVisible = true;
             },
             getCurrentPrice(prices) {
-                return prices[this.selectedCurrency] ? prices[this.selectedCurrency] : 0
+                return prices[this.selectedCurrency] ? prices[this.selectedCurrency] : 0;
             },
             init() {
-                this.getList()
+                this.getList();
             },
             async getList() {
                 if (this.category) {
-                    const { data } = await fetchList({ search: 'category_id:' + this.category.id })
-                    this.items = data
+                    const { data } = await fetchList({
+                        search: "category_id:" + this.category.id
+                    });
+                    this.items = data;
                 }
             },
             confirmCart() {
-                this.disableAddCart = true
-                this.addToCart(this.form)
+                this.disableAddCart = true;
+                this.addToCart(this.form);
                 this.$message({
                     showClose: true,
-                    message: 'Congratulations! You have successfully added that item to your cart and ready to check out.',
-                    type: 'success'
+                    message:
+                        "Congratulations! You have successfully added that item to your cart and ready to check out.",
+                    type: "success"
                 });
-                setTimeout(() => this.centerDialogVisible = false, 2000);
-                setTimeout(() => this.disableAddCart = false, 2000);
+                setTimeout(() => (this.centerDialogVisible = false), 2000);
+                setTimeout(() => (this.disableAddCart = false), 2000);
             }
         },
         watch: {
             form(value) {
-                this.totalPrice =  this.getCurrentPrice(value.prices)
+                this.totalPrice = this.getCurrentPrice(value.prices);
             },
-            'form.selectedItem' (value) {
-                this.totalPrice = value * this.getCurrentPrice(this.form.prices)
+            "form.selectedItem"(value) {
+                this.totalPrice = value * this.getCurrentPrice(this.form.prices);
             },
             num() {
-                this.form.qty = this.num
+                this.form.qty = this.num;
             }
         }
-    }
+    };
 </script>
 
 <style scoped>
@@ -224,16 +226,14 @@
         font-weight: 400;
         margin: 0px;
     }
-    .main-item span{
+    .main-item span {
         overflow: hidden;
         white-space: pre-line;
-
     }
     .main-item h2 {
         color: rgb(0, 0, 0);
         font-size: 20px;
         line-height: 24px;
-        font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         font-weight: 500;
         margin: 8px 0px;
     }
@@ -241,7 +241,6 @@
         padding-right: 4px;
         font-size: 22px;
         color: rgb(0, 0, 0);
-        font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         font-weight: 500;
         line-height: 42px;
         flex: 1 1 auto;
@@ -252,10 +251,10 @@
         margin: 0px;
     }
     .pizza-footer {
-        display: flex; margin-top: 16px;
+        display: flex;
+        margin-top: 16px;
     }
     .pizza-select button {
-        font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         font-weight: 500;
         font-size: 14px;
         height: 40px;
@@ -279,5 +278,10 @@
     }
     .size-select {
         margin: 15px 0 15px 0;
+    }
+    @media only screen and (max-width: 600px) {
+        .dialog-mobile >>> .el-dialog {
+            width: 90% !important;
+        }
     }
 </style>
